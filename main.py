0ba63cdaa2417cf1df9632f691201aecbe925b73
Alexandr <s.stepantzover@yandex.ru>
2022-04-26 19:19:28 +0300

    ADD ZAPISKA

--- main.py
+++ main.py
@@ -179,6 +179,38 @@ def convert_price(value):
     return price * value
 
 
+'''
+        Основная функция,реализация критерия 
+    '''
+
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+
+'''
+        Основная функция,реализация критерия 
+    '''
+
+
 def convert_price_eur(value):
     '''
         функция, в которой происходит конверт валют
